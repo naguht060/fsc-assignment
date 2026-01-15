23beabf837fd15799954f72468a59eb09f771b52
naguht060 <nagaraj.tulasigeri@gmail.com>
2026-01-15 22:17:46 +0530

    Updated pom.xml file

--- src/test/java/com/fulfilment/application/monolith/warehouses/adapters/database/FulfilmentAssServiceCompTest.java
+++ src/test/java/com/fulfilment/application/monolith/warehouses/adapters/database/FulfilmentAssServiceCompTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
-public class FulfilmentAssignmentServiceComprehensiveTest {
+public class FulfilmentAssServiceCompTest {
 
   @Inject FulfilmentAssignmentService fulfilmentAssignmentService;
   @Inject ProductRepository productRepository;
